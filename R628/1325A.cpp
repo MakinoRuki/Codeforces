160bf702ad1a7916aaf58ea4fd96c5a9963c74b0
MakinoRuki <muye67@gmail.com>
2020-03-16 23:41:56 -0700

    Create 1325A.cpp

---
+++ R628/1325A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <deque>
+#define N 1000005
+using namespace std;
+typedef long long ll;
+int t, x;
+int main() {
+	cin>>t;
+	for (int cas=1; cas<=t; ++cas) {
+		cin>>x;
+		cout<<1<<" "<<x-1<<endl;
+	}
+	return 0;
+}
