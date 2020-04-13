bbf5baf0a88e0423fe9e1278200f349168cf9ca8
MakinoRuki <muye67@gmail.com>
2020-04-12 22:29:23 -0700

    Create 1339A.cpp

---
+++ CF633/1339A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <deque>
+#include <queue>
+#define N 5002
+using namespace std;
+typedef long long ll;
+int t,n;
+int main() {
+	cin>>t;
+	for (int cas=1; cas<=t; ++cas) {
+		scanf("%d", &n);
+		cout<<n<<endl;
+	}
+	return 0;
+}
