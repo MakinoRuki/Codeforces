fd68668756a2c81fadbc9940eacb5a8a118989ce
MakinoRuki <muye67@gmail.com>
2020-04-16 01:52:42 -0700

    Create 1337A.cpp

---
+++ R635/1337A.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <deque>
+#include <queue>
+#define N 400005
+using namespace std;
+typedef long long ll;
+int t, n, m;
+int a, b, c, d;
+int main() {
+	cin>>t;
+	for (int cas=1; cas<=t; ++cas) {
+		cin>>a>>b>>c>>d;
+		cout<<b<<" "<<c<<" "<<c<<endl;
+	}
+	return 0;
+}
