c21391633462daa472f75b8212c3329729eda2f9
MakinoRuki <muye67@gmail.com>
2020-06-20 23:10:33 -0700

    Create 1370A.cpp

---
+++ R651/1370A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#define N 100005
+#define M 100005
+#define inf 1000000000
+using namespace std;
+typedef long long ll;
+const ll mod = 998244353LL;
+int t, n;
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    cin>>n;
+    cout<<n/2<<endl;
+  }
+  return 0;
+}
