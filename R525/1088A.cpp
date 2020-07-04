5eac06dffd7bb16db29e957ccfeff96e5855db34
MakinoRuki <muye67@gmail.com>
2020-07-04 16:23:31 -0700

    Create 1088A.cpp

---
+++ R525/1088A.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#define N 105
+#define M 10005
+#define inf 1000000000
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+int n, k;
+int main() {
+  int x;
+  cin>>x;
+  if (x==1) cout<<-1<<endl;
+  else cout<<x<<" "<<x<<endl;
+  return 0;
+}
