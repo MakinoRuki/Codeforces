eaaf2046ce3922fb77b3289ffcfd57bdd4eac9c7
MakinoRuki <muye67@gmail.com>
2020-08-17 09:08:30 -0700

    Create 1312B.cpp

---
+++ ER83/1312B.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#define N 205
+//#define M 10005
+#define inf 1000000000
+#define pii pair<int, int>
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+//const ll mod2 = 999999937LL;
+int t, n, m;
+int a[N];
+int main() {
+	cin>>t;
+	for (int cas=1; cas<=t; ++cas) {
+		cin>>n;
+		for (int i = 1; i <= n; ++i) {
+			scanf("%d", &a[i]);
+		}
+		sort(a+1, a+n+1);
+		for (int i = n; i >= 1; --i) {
+			printf("%d", a[i]);
+			if (i > 1) cout<<" ";
+			else cout<<endl;
+		}
+	}
+	return 0;
+}
