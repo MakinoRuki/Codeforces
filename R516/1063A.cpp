7865a7d801867bf9871e8f5dbc41b006724c8963
MakinoRuki <muye67@gmail.com>
2020-08-28 00:03:55 -0700

    Create 1063A.cpp

---
+++ R516/1063A.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#define N 5002
+//#define M 10005
+#define inf 1000000000
+#define pii pair<int, int>
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+int n;
+int a[N];
+string s;
+int main() {
+	cin>>n>>s;
+	sort(s.begin(), s.end());
+	cout<<s<<endl;
+	return 0;
+}
