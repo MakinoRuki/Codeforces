332e9413a67102a895644e439bc70db6f12c7475
MakinoRuki <muye67@gmail.com>
2020-12-02 21:29:48 -0800

    Create 1455A.cpp

---
+++ ER99/1455A.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <deque>
+#include <cmath>
+#include <queue>
+#define eps 1e-7
+#define M 20005
+#define N 100005
+#define SZ 200000002
+using namespace std;
+typedef long long ll;
+const int inf = 1000000000;
+ll mod = 1000000007LL;
+ll mod2 = 998244353LL;
+int t;
+string n;
+int main() {
+	cin>>t;
+	for (int cas=1; cas<=t; ++cas) {
+		cin>>n;
+		int res=(int)n.size();
+		printf("%d\n", res);
+	}
+	return 0;
+}
