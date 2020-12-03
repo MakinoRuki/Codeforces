1c6845335d700d3a34b2fa3b4de296eebce5c66b
MakinoRuki <muye67@gmail.com>
2020-12-02 21:12:44 -0800

    Create 1107B.cpp

---
+++ ER59/1107B.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <deque>
+#include <cmath>
+#include <queue>
+#define eps 1e-7
+#define M 20005
+#define N 200005
+#define SZ 200000002
+using namespace std;
+typedef long long ll;
+const int inf = 1000000000;
+ll mod = 1000000007LL;
+ll mod2 = 998244353LL;
+int t, n, m;
+int q;
+int main() {
+	cin>>n;
+	for (int cas=1; cas<=n; ++cas) {
+		ll k;
+		int x;
+		scanf("%lld%d",&k,&x);
+		ll res=(k-1)*9+x;
+		printf("%lld\n", res);
+	}
+	return 0;
+}
