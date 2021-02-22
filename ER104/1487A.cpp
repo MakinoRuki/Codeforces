e427ed52b93e3d7a28000d8ccc2de62da34a862f
MakinoRuki <muye67@gmail.com>
2021-02-21 19:32:48 -0800

    Create 1487A.cpp

---
+++ ER104/1487A.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <deque>
+#include <cmath>
+#include <queue>
+#define eps 1e-7
+#define M 21
+#define N 200
+#define SZ 200000002
+using namespace std;
+typedef long long ll;
+const int inf = 1000000000;
+ll mod = 1000000007LL;
+ll mod2 = 998244353LL;
+int n, m;
+int t;
+int a[N];
+int main() {
+	cin>>t;
+	for (int cas=1; cas<=t; ++cas) {
+		scanf("%d", &n);
+		for (int i = 1; i <= n; ++i) {
+			scanf("%d", &a[i]);
+		}
+		sort(a+1, a+n+1);
+		int i;
+		for (i = 1; i <= n; ++i) {
+			if (a[i] != a[1]) break;
+		}
+		int res=max(0,n-i+1);
+		printf("%d\n", res);
+	}
+  return 0;
+}
