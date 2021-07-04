3bddf39f84c963cd56af7e323c8f2c4e1f2ed170
MakinoRuki <muye67@gmail.com>
2021-07-04 00:00:39 -0700

    Create 766A.cpp

---
+++ R396/766A.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <deque>
+#include <cmath>
+#include <queue>
+#include <time.h>
+#define eps 1e-7
+#define M 22
+#define N 1000005
+#define SZ 200000002
+using namespace std;
+typedef long long ll;
+const int inf = 1000000000;
+ll mod = 1000000007LL;
+ll mod2 = 998244353LL;
+int t, n;
+int k;
+int q;
+string a, b;
+int main() {
+	cin>>a>>b;
+	if (a == b) {
+		puts("-1");
+	} else {
+		printf("%d\n", (int)max(a.size(), b.size()));
+	}
+  return 0;
+}
