3e5e49a4d948ce782133301b7703fd58e7c08bf2
MakinoRuki <muye67@gmail.com>
2021-08-17 22:25:53 -0700

    Create 1549A.cpp

---
+++ R736/1549A.cpp
@@ -0,0 +1,32 @@
+#pragma GCC optimize(2)
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <deque>
+#include <cmath>
+#include <queue>
+#include <time.h>
+#define eps 1e-7
+#define M 22
+#define N 300005
+#define SZ 200000002
+using namespace std;
+typedef long long ll;
+const int inf = 1000000000;
+ll mod = 1000000007LL;
+ll mod2 = 998244353LL;
+int t, n, m, k;
+int main() {
+	cin>>t;
+	for (int cas=1; cas<=t; ++cas) {
+		scanf("%d", &n);
+		printf("%d %d\n", 2, n-1);
+	}
+  return 0;
+}
