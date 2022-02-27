67a6a51a00dd3e5846107eb5913b2dbc930e124c
MakinoRuki <muye67@gmail.com>
2022-02-27 13:17:34 -0800

    Update 794B.cpp

--- R414/794B.cpp
+++ R414/794B.cpp
@@ -17,79 +17,14 @@ const ll mod = 1000000007LL;
 const ll mod2 = 998244353LL;
 int n, m, k, q;
 int t;
-string s1, s2;
-int cnt1[26], cnt2[26];
 int main() {
-  cin>>s1>>s2;
-  memset(cnt1, 0, sizeof(cnt1));
-  memset(cnt2, 0, sizeof(cnt2));
-  // cin>>n;
-  // for (int i = 0; i < n; ++i) {
-  //   s1.push_back(rand()%26+'a');
-  //   s2.push_back(rand()%26+'a');
-  // }
-  // cout<<s1<<endl<<s2<<endl;
-  n = (int)s1.size();
-  for (int i = 0; i < n; ++i) {
-    cnt1[s1[i]-'a']++;
-    cnt2[s2[i]-'a']++;
+  int h;
+  cin>>n>>h;
+  for (int i = 1; i < n; ++i) {
+    double res = sqrt((double)h*(double)h*(double)i/(double)n);
+    printf("%.10lf", res);
+    if (i < n-1) cout<<" ";
+    else cout<<endl;
   }
-  sort(s1.begin(), s1.end());
-  sort(s2.begin(), s2.end());
-  string ans = string(n, ' ');
-  int l = 0, r = n-1;
-  for (int i = 0; i < n; ++i) {
-    if (i % 2 == 0) {
-      int j1, j2;
-      for (j1 = 0; j1 < 26; ++j1) {
-        if (cnt1[j1]) break;
-      }
-      for (j2 = 25; j2 >= 0; --j2) {
-        if (cnt2[j2]) break;
-      }
-      if (j1 >= j2) {
-        int det = (n-i+1)/2;
-        int tot=0;
-        for (j1 = 0; j1 < 26; ++j1) {
-          tot += cnt1[j1];
-          if (tot >= det){
-            break;
-          }
-        }
-     //   cout<<"j1="<<r<<" "<<j1<<endl;
-        ans[r--] = 'a' + j1;
-        cnt1[j1]--;
-      } else {
-        ans[l++] = 'a' + j1;
-        cnt1[j1]--;
-      }
-    } else {
-      int j1, j2;
-      for (j1 = 0; j1 < 26; ++j1) {
-        if (cnt1[j1]) break;
-      }
-      for (j2 = 25; j2 >= 0; --j2) {
-        if (cnt2[j2]) break;
-      }
-      if (j1 >= j2) {
-        int det = (n-i+1)/2;
-        if (det == 0) continue;
-        int tot=0;
-        for (j2 = 25; j2 >= 0; --j2) {
-          tot += cnt2[j2];
-          if (tot >= det){
-            break;
-          }
-        }
-     //   cout<<"j2="<<r<<" "<<j2<<endl;
-        ans[r--] = 'a' + j2;
-        cnt2[j2]--;
-      } else {
-        ans[l++] = 'a' + j2;
-        cnt2[j2]--;
-      }
-    }
-  }
-  cout<<ans<<endl;
   return 0;
 }
