cdb06fd49aad2ac576f15001108efdd5a6f9be55
MakinoRuki <muye67@gmail.com>
2022-03-14 09:23:56 -0700

    Create 1651A.cpp

---
+++ ER124/1651A.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#define M 202
+#define N 100005
+#define inf 1000000000
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+int n, m, k, q;
+int t;
+string s;
+int a[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d", &n);
+    ll res = (1LL<<n) - 1;
+    printf("%lld\n", res);
+  }
+  return 0;
+}
