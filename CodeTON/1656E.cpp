eb0e07df80a9be2664180baf874b9b6dbc200ca8
MakinoRuki <muye67@gmail.com>
2022-03-27 21:48:30 -0700

    Create 1656E.cpp

---
+++ CodeTON/1656E.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#define M 202
+#define N 200005
+#define inf 1000000000
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+int m, k, q;
+int n;
+int t;
+int c;
+vector<int> edges[N];
+int col[N];
+void dfs(int u, int p, int c) {
+  col[u] = c;
+  for (int i = 0; i < edges[u].size(); ++i) {
+    int v = edges[u][i];
+    if (v == p) continue;
+    dfs(v, u, !c);
+  }
+}
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d", &n);
+    for (int i = 1; i <= n; ++i) {
+      edges[i].clear();
+      col[i] = -1;
+    }
+    for (int i = 1; i < n; ++i) {
+      int u, v;
+      scanf("%d%d", &u, &v);
+      edges[u].push_back(v);
+      edges[v].push_back(u);
+    }
+    dfs(1, 0, 0);
+    for (int i = 1; i <= n; ++i) {
+      if (col[i]) {
+        printf("%d ", (int)edges[i].size());
+      } else {
+        printf("%d ", -(int)edges[i].size());
+      }
+    }
+    cout<<endl;
+  }
+  return 0;
+}
