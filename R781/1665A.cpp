e57ed899a805887a0ff5a473bb65b33438bf8e46
MakinoRuki <muye67@gmail.com>
2022-04-17 18:44:32 -0700

    Create 1665A.cpp

---
+++ R781/1665A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#define N 500005
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+int n, m;
+int t;
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d", &n);
+    printf("%d %d %d %d\n", n-3, 1, 1, 1);
+  }
+  return 0;
+}
