b55fc4c2909b99756e73f4acb67e12e47874e217
MakinoRuki <muye67@gmail.com>
2022-06-28 22:31:42 -0700

    Create 805C.cpp

---
+++ R411/805C.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <queue>
+#include <cmath>
+#define N 500
+#define inf 1000000000
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+int n, m, k, q;
+int t;
+int main() {
+  cin>>n;
+  cout<<(n-1)/2<<endl;
+  return 0;
+}
