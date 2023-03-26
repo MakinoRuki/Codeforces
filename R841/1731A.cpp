3d2749e7e1c6b3e1cc170954a8f7eca29eb64a22
MakinoRuki <muye67@gmail.com>
2023-03-26 03:49:28 -0700

    Create 1731A.cpp

---
+++ R841/1731A.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdio>
+#include <stdio.h>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <cstring>
+#define N 100
+#define M 5005
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const int inf = 1000000000;
+int n, m, k, q, p;
+int t;
+ll a[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d", &n);
+    ll tot = 1LL;
+    for (int i = 1; i <= n; ++i) {
+      scanf("%lld", &a[i]);
+      tot = tot * a[i];
+    }
+    tot += n-1;
+    tot *= 2022LL;
+    printf("%lld\n", tot);
+  }
+  return 0;
+}
