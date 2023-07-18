5a8b906f7b2d52a2a1c6e0889818a326fc1ca128
MakinoRuki <muye67@gmail.com>
2023-07-18 04:23:21 -0700

    Create 1843A.cpp

---
+++ R881/1843A.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#define N 600005
+#define M 22
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const int inf = 1000000010;
+int n, m, k, q;
+int t;
+int x;
+int a[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d", &n);
+    for (int i = 1; i <= n; ++i) {
+      scanf("%d", &a[i]);
+    }
+    sort(a+1, a+n+1);
+    int  l = 1, r = n;
+    int tot=0;
+    while(l < r) {
+      tot += a[r] - a[l];
+      l++;
+      r--;
+    }
+    printf("%d\n", tot);
+  }
+  return 0;
+}
