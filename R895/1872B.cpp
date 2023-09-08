9a23b37ecd177720beda48fb68ac920edd070a34
MakinoRuki <muye67@gmail.com>
2023-09-08 06:04:12 -0700

    Create 1872B.cpp

---
+++ R895/1872B.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#define N 200
+#define M 100
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const int inf = 1000000010;
+int n, m, k, q;
+int t;
+int x;
+int d[N], s[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d", &n);
+    for (int i = 1; i <= n; ++i) {
+      scanf("%d%d", &d[i], &s[i]);
+    }
+    int ans=1000000000;
+    for (int i = 1; i <= n; ++i) {
+      ans = min(ans, d[i]+(s[i]+1)/2-1);
+    }
+    printf("%d\n", ans);
+  }
+  return 0;
+}
