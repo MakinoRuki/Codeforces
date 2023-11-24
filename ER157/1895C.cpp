981b695dc14bb5893a1048826469156117735cf1
MakinoRuki <muye67@gmail.com>
2023-11-23 23:50:42 -0800

    Update 1895C.cpp

--- ER157/1895C.cpp
+++ ER157/1895C.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdio>
+#include <stdio.h>
 #include <cstring>
 #include <vector>
 #include <algorithm>
@@ -10,46 +11,64 @@
 #include <queue>
 #include <random>
 #include <ctime>
-#define N 300005
-#define M 20
+#include <cstring>
+#define N 2000005
+#define M 5005
 #define eps 1e-7
 using namespace std;
 typedef long long ll;
 const ll mod = 1000000007LL;
 const ll mod2 = 998244353LL;
-const int inf = 1000000010;
-int n, m, k, q;
+const int inf = 1000000000;
+int n, m, k, q, p;
 int t;
 int a[N];
-int b[N];
+ll ans;
+int cnt[12][100];
 int main() {
-  scanf("%d", &n);
-  for (int i = 1; i < n; ++i) {
-    scanf("%d", &a[i]);
-  }
-  int res=a[1];
-  for (int i = 2; i <= n; ++i) {
-    b[i] = res;
-    res ^= a[i];
-  }
-  b[1] = 0;
-  for (int i = 20; i >= 0; --i) {
-    int cnt=0;
-    for (int j = 0; j < n; ++j) {
-      if ((1<<i) & j) cnt++;
-    }
-    int cnt2=0;
-    for (int j = 2; j <= n; ++j) {
-      if ((1<<i) & b[j]) cnt2++;
-    }
-    if (cnt != cnt2) {
-      b[1] |= (1<<i);
-    }
-  }
+  cin>>n;
+  vector<string> sv;
+  sv.clear();
+  memset(cnt, 0, sizeof(cnt));
   for (int i = 1; i <= n; ++i) {
-    if (i > 1) b[i] ^= b[1];
-    printf("%d ", b[i]);
+    string s;
+    cin>>s;
+    sv.push_back(s);
+    int len = (int)s.size();
+    int sum=0;
+    for (int j = 0; j < len; ++j) {
+      sum += s[j]-'0';
+    }
+    cnt[len][sum]++;
   }
-  cout<<endl;
+  ll ans = 0LL;
+  for (int i = 2; i <= 10; i += 2) {
+    for (int j = 0; j < sv.size(); ++j) {
+      int len = sv[j].size();
+      if (len >= i/2 && len < i) {
+        int sum = 0;
+        int sum2 = 0;
+        for (int k = 0; k < i/2; ++k) {
+          sum += sv[j][k]-'0';
+        }
+        for (int k = i/2; k < sv[j].size(); ++k) {
+          sum2 += sv[j][k] - '0';
+        }
+        if (sum - sum2 >= 0) ans += cnt[i-len][sum-sum2];
+      }
+      if (len > i/2 && len < i) {
+        int sum=0;
+        int sum2=0;
+        for (int k = 0; k < len - i/2; ++k) {
+          sum2 += sv[j][k]-'0';
+        }
+        for (int k = len-i/2; k < len; ++k) {
+          sum += sv[j][k]-'0';
+        }
+        if (sum-sum2 >= 0) ans += cnt[i-len][sum-sum2];
+      }
+    }
+  }
+  printf("%lld\n", ans);
   return 0;
 }
