5a99b88e69c7fbf733a39869cc696c40a823b59b
MakinoRuki <muye67@gmail.com>
2023-12-06 06:15:29 -0800

    Create 1747B.cpp

---
+++ R832/1747B.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#define N 5002
+#define M 1502
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const int inf = 1000000010;
+int n, m, k, q;
+int t;
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d", &n);
+    printf("%d\n", (n+1)/2);
+    int l = 0, r = n-1;
+    while(l <=r) {
+      printf("%d %d\n", l*3+1, r*3+2);
+      l++;
+      r--;
+    }
+  }
+  return 0;
+}
