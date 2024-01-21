8551f8ff38af47caf01f2a43aac65cd9eb596b32
MakinoRuki <muye67@gmail.com>
2024-01-20 18:42:11 -0800

    Create 1717A.cpp

---
+++ R818/1717A.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#define N 500005
+#define M 1502
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const int inf = 1000000010;
+int n, m, k, q;
+int t;
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d", &n);
+    ll tot = n + n/2 + n/3;
+    tot *= 2;
+    tot -= n;
+    printf("%lld\n", tot);
+  }
+  return 0;
+}
