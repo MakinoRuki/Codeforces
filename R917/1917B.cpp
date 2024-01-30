a9667b0b6831ea9dd2d78cafd48b53c843f01d2f
MakinoRuki <muye67@gmail.com>
2024-01-30 05:41:28 -0800

    Create 1917B.cpp

---
+++ R917/1917B.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#define N 300005
+#define M 20
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const int inf = 1000000010;
+int n, m, k, q;
+int t;
+int a[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d", &n);
+    string s;
+    cin>>s;
+    set<int> ss;
+    ss.clear();
+    ll ans=0LL;
+    for (int i = 0; i < n; ++i) {
+      int sz=(int)ss.size();
+      ans += sz;
+      ss.insert(s[i]-'a');
+    }
+    ans += (int)ss.size();
+    printf("%lld\n", ans);
+  }
+  return 0;
+}
