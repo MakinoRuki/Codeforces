e5ca0f93eacbeb9715014571e1e0a0369598b730
MakinoRuki <muye67@gmail.com>
2024-02-24 06:12:14 -0800

    Create 1789A.cpp

---
+++ R853/1789A.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#define N 500005
+#define M 15
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const int inf = 1000000010;
+int n, m, k, q;
+int t;
+int a[N];
+int getgcd(int x, int y) {
+  return y == 0 ? x : getgcd(y, x%y);
+}
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d", &n);
+    for (int i = 1; i <= n; ++i) {
+      scanf("%d", &a[i]);
+    }
+    bool found=false;
+    for (int i = 1; i <= n; ++i) {
+      for (int j = i+1; j <=n; ++j) {
+        if (getgcd(a[i], a[j]) <= 2) {
+          found=true;
+          break;
+        }
+      }
+    }
+    if (found)puts("YES");
+    else puts("NO");
+  }
+  return 0;
+}
