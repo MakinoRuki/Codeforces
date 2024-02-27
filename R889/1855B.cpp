3734bcc1facdf541be4222b28cab78d89cc32072
MakinoRuki <muye67@gmail.com>
2024-02-27 06:26:29 -0800

    Create 1855B.cpp

---
+++ R889/1855B.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#define N 300005
+#define M 1502
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const int inf = 1000000010;
+int n, m, k, q;
+int t;
+int a[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    ll n;
+    scanf("%lld", &n);
+    ll ans;
+    for (ans = 1; ans <= 50; ++ans) {
+      if (n % ans) {
+        break;
+      }
+    }
+    ans--;
+    printf("%lld\n", ans);
+  }
+  return 0;
+}
