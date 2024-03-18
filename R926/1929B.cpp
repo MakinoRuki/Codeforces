f04f3496b68bbf0f1f33b92b67c4adb9afc8986e
MakinoRuki <muye67@gmail.com>
2024-03-18 09:26:08 -0700

    Create 1929B.cpp

---
+++ R926/1929B.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#define N 300005
+#define M 1005
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const int inf = 1000000010;
+int n, m, k, q;
+int t;
+int a[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d%d", &n, &k);
+    int dt=(k+1)/2;
+    if (k == 4*n-2) {
+      dt++;
+    }
+    printf("%d\n", dt);
+  }
+  return 0;
+}
