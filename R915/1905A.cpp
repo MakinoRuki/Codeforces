a311f4dfa59360dc6ebedbe28cb52b3f9524d5c1
MakinoRuki <muye67@gmail.com>
2024-04-04 05:23:21 -0700

    Create 1905A.cpp

---
+++ R915/1905A.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#define N 200005
+#define M 635
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const int inf = 1000000010;
+int n, m, k, q;
+int t;
+int a[N];
+int b[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d%d", &n, &m);
+    int res=max(n,m);
+    printf("%d\n", res);
+  }
+  return 0;
+}
