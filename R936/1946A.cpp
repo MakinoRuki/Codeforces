96323e626f224bd7793f576ba91b0d4fd809eee6
MakinoRuki <muye67@gmail.com>
2024-04-05 08:57:25 -0700

    Create 1946A.cpp

---
+++ R936/1946A.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#define N 1000005
+#define M 635
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const int inf = 1000000010;
+int n, m, k, q;
+int t;
+int a[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d", &n);
+    for (int i = 1; i <= n; ++i) {
+      scanf("%d", &a[i]);
+    }
+    sort(a+1, a+n+1);
+    int cnt=0;
+    for (int i = (n+1)/2; i <= n; ++i) {
+      if (a[i] == a[(n+1)/2]) {
+        cnt++;
+      } else {
+        break;
+      }
+    }
+    printf("%d\n", cnt);
+  }
+  return 0;
+}
