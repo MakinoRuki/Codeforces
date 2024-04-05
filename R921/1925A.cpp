2dbaa6956b3663786f5f6cc31bdfeedc593e9c09
MakinoRuki <muye67@gmail.com>
2024-04-05 08:19:19 -0700

    Create 1925A.cpp

---
+++ R921/1925A.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#define N 1000005
+#define M 635
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const int inf = 1000000010;
+int n, m, k, q;
+int t;
+int a[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d%d", &n, &k);
+    string ans="";
+    for (int i = 1; i <= n; ++i) {
+      for (int j = 0; j < k; ++j) {
+        ans.push_back('a'+j);
+      }
+    }
+    cout<<ans<<endl;
+  }
+  return 0;
+}
