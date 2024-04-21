a7dfa4f7e86762fc9138d476afaf3f9c05137a72
MakinoRuki <muye67@gmail.com>
2024-04-21 08:54:01 -0700

    Create 1954A.cpp

---
+++ ER164/1954A.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#define N 1000005
+#define M 635
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const int inf = 1000000010;
+const __int128 hmod = 212370440130137957LL;
+const int base=337;
+int n, m, k, q;
+int t;
+int a[M];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d%d%d", &n, &m, &k);
+    if (n == k) {
+      puts("No");
+    } else {
+      if (m*(n-k-1) >= n) {
+        puts("Yes");
+      } else {
+        puts("No");
+      }
+    }
+  }
+  return 0;
+}
