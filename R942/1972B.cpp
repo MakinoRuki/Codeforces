885d1664c72e6c52152775402ca84ee6ae1bfef9
MakinoRuki <muye67@gmail.com>
2024-05-13 17:30:27 +0900

    Create 1972B.cpp

---
+++ R942/1972B.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#define N 200005
+#define M 5005
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const int inf = 1000000010;
+const __int128 hmod = 212370440130137957LL;
+const int base=337;
+int n, m, k, q;
+int t;
+int a[N];
+int b[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d", &n);
+    string s;
+    cin>>s;
+    int cnt=0;
+    for (int i=  0; i < n; ++i) {
+      if (s[i] == 'U') cnt++;
+    }
+    if (cnt%2) puts("YES");
+    else puts("NO");
+  }
+  return 0;
+}
