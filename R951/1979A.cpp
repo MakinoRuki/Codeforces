68544c551525df596dc5fb62edf8f7169da88d03
MakinoRuki <muye67@gmail.com>
2024-06-26 05:39:40 -0700

    Create 1979A.cpp

---
+++ R951/1979A.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#define N 500005
+#define M 5005
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+const int base=337;
+int n, m, k, q;
+int t;
+int a[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d", &n);
+    for (int i = 1; i <= n; ++i) {
+      scanf("%d", &a[i]);
+    }
+    int res=max(a[1], a[2]);
+    for (int i = 2; i < n; ++i) {
+      res = min(res, max(a[i], a[i+1]));
+    }
+    printf("%d\n", res-1);
+  }
+  return 0;
+}
