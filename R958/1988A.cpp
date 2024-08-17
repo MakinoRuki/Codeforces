42c6193acae92df894c751d046a356c0b237bd19
MakinoRuki <muye67@gmail.com>
2024-08-17 08:17:39 -0700

    Create 1988A.cpp

---
+++ R958/1988A.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#define N 500005
+#define M 505
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+const int base=337;
+int n, m, k, q;
+int t;
+int a[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d%d", &n, &k);
+    int res = (n-1+k-2)/(k-1);
+    printf("%d\n", res);
+  }
+  return 0;
+}
