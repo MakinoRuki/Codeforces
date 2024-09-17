e07e676d9735e6a3601e2fcc67939dabb21a6932
MakinoRuki <muye67@gmail.com>
2024-09-16 20:42:36 -0700

    Create 2007A.cpp

---
+++ R969/2007A.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#define N 500005
+#define M 505
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0)); 
+const int base=337;
+int n, m, k, q;
+int t;
+int a[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    int l, r;
+    scanf("%d%d", &l, &r);
+    int len=r-l+1;
+    if (len < 3) {
+      puts("0");
+      continue;
+    }
+    int res=0;
+    if (l%2) {
+      res = (len-3)/4+1;
+    } else {
+      res = len/4;
+    }
+    printf("%d\n", res);
+  }
+  // cin>>n;
+  // srand((unsigned)time(0)); 
+  // for (int i = 1; i <= n; ++i) {
+  //   printf("%d ", rand()%n+1);
+  // }
+  // cout<<endl;
+  return 0;
+}
+
