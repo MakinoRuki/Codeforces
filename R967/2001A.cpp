e33dec5771778361d14231f5a44d014deb1ac8f9
MakinoRuki <muye67@gmail.com>
2024-09-16 19:00:47 -0700

    Create 2001A.cpp

---
+++ R967/2001A.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#define N 300005
+#define M 505
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0)); 
+const int base=337;
+int n, m, k, q;
+int t;
+int a[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d", &n);
+    map<int,int> cnt;
+    cnt.clear();
+    for (int i = 1; i <= n; ++i) {
+      scanf("%d", &a[i]);
+      cnt[a[i]]++;
+    }
+    int mx=0;
+    for (auto itr : cnt) {
+      mx = max(mx, itr.second);
+    }
+    printf("%d\n", n-mx);
+  }
+  // cin>>n;
+  // srand((unsigned)time(0)); 
+  // for (int i = 1; i <= n; ++i) {
+  //   printf("%d ", rand()%n+1);
+  // }
+  // cout<<endl;
+  return 0;
+}
+
