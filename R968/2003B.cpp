f59ee2a97d355da08aa6fa630152635695ba8270
MakinoRuki <muye67@gmail.com>
2024-09-16 19:54:25 -0700

    Create 2003B.cpp

---
+++ R968/2003B.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#define N 300005
+#define M 505
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0)); 
+const int base=337;
+int n, m, k, q;
+int t;
+int a[N];
+int par[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d", &n);
+    for (int i = 1; i <= n; ++i) {
+      scanf("%d", &a[i]);
+    }
+    sort(a+1, a+n+1);
+    if (n%2) {
+      printf("%d\n", a[(n+1)/2]);
+    } else {
+      printf("%d\n", a[n/2+1]);
+    }
+  }
+  // cin>>n;
+  // srand((unsigned)time(0)); 
+  // for (int i = 1; i <= n; ++i) {
+  //   printf("%d ", rand()%n+1);
+  // }
+  // cout<<endl;
+  return 0;
+}
+
