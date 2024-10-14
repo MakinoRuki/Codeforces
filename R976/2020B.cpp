2fc3fb9bb61e3a54f1df10ccef9fd502e9094bbf
MakinoRuki <muye67@gmail.com>
2024-10-14 04:59:40 -0700

    Create 2020B.cpp

---
+++ R976/2020B.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#define N 500005
+#define M 505
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0)); 
+const int base=337;
+int n, m, k, q;
+int t;
+int a[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    ll k;
+    scanf("%lld", &k);
+    ll l = 1, r = 2000000000000000000LL;
+    while(l < r) {
+      ll mid = (l+r)/2;
+      ll qr = (ll)sqrt(mid);
+      if (mid - qr >= k) {
+        r = mid;
+      } else {
+        l = mid + 1;
+      }
+    }
+    printf("%lld\n", r);
+  }
+  // cin>>n;
+  // srand((unsigned)time(0)); 
+  // for (int i = 1; i <= n; ++i) {
+  //   printf("%d ", rand()%n+1);
+  // }
+  // cout<<endl;
+  return 0;
+}
+
