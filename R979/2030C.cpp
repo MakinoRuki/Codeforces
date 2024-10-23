6e3478262c0d09f4d019a33901b6ef72630e1733
MakinoRuki <muye67@gmail.com>
2024-10-23 06:22:57 -0700

    Create 2030C.cpp

---
+++ R979/2030C.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#define N 500005
+#define M 325
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0)); 
+const int base=337;
+int n, m, k, q;
+int t;
+int a[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d", &n);
+    string s;
+    cin>>s;
+    bool found=false;
+    for (int i = 0; i < n-1; ++i) {
+      if (s[i] == '1' && s[i+1] == '1') {
+        found=true;
+        break;
+      }
+    }
+    if (s[0] == '1' || s[n-1] == '1') {
+      found=true;
+    }
+    if (found) puts("YES");
+    else puts("NO");
+  }
+  // cin>>n;
+  // srand((unsigned)time(0)); 
+  // for (int i = 1; i <= n; ++i) {
+  //   printf("%d ", rand()%n+1);
+  // }
+  // cout<<endl;
+  return 0;
+}
+
