4f72ad7d6c33c9bacc693d0935a84e596a0af15e
MakinoRuki <muye67@gmail.com>
2024-11-09 10:57:44 -0800

    Create 2032A.cpp

---
+++ R983/2032A.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 500005
+#define M 405
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0)); 
+const int base=337;
+int n, m, k, p;
+int t;
+int a[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d", &n);
+    int n0=0, n1=0;
+    for (int i= 1; i <= 2*n; ++i) {
+      scanf("%d", &a[i]);
+      if (a[i]) n1++;
+      else n0++;
+    }
+    printf("%d %d\n", ((n0%2)?1:0), min(n0,n1));
+  }
+  // cin>>n;
+  // srand((unsigned)time(0)); 
+  // for (int i = 1; i <= n; ++i) {
+  //   printf("%d ", rand()%n+1);
+  // }
+  // cout<<endl;
+  return 0;
+}
