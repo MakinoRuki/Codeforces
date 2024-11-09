fa534bc1206ce93f88c13530999b72c465d297b6
MakinoRuki <muye67@gmail.com>
2024-11-09 10:50:34 -0800

    Create 2027A.cpp

---
+++ R982/2027A.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 500005
+#define M 405
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0)); 
+const int base=337;
+int n, m, k, p;
+int t;
+int a[M];
+vector<pair<int,int>> wh;
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d", &n);
+    wh.clear();
+    int mw=0,mh=0;
+    for (int i = 1; i <= n; ++i) {
+      int w,h;
+      scanf("%d%d", &w, &h);
+      mw = max(mw, w);
+      mh = max(mh, h);
+    }
+    printf("%d\n", 2*mw + 2*mh);
+  }
+  // cin>>n;
+  // srand((unsigned)time(0)); 
+  // for (int i = 1; i <= n; ++i) {
+  //   printf("%d ", rand()%n+1);
+  // }
+  // cout<<endl;
+  return 0;
+}
+
