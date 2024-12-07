0b7a58c2eaadbfe52bd013c85d536c43468eae67
MakinoRuki <muye67@gmail.com>
2024-12-06 22:38:57 -0800

    Create 2034A.cpp

---
+++ R989/2034A.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 500005
+#define M 405
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0)); 
+const int base=337;
+int n, m;
+ll k;
+int t;
+int a[N];
+int getgcd(int x, int y) {
+  return y == 0 ? x : getgcd(y, x%y);
+}
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    int x,y;
+    scanf("%d%d", &x, &y);
+    int g = getgcd(x, y);
+    int res = x/g*y;
+    printf("%d\n", res);
+  }
+  // cin>>n;
+  // srand((unsigned)time(0)); 
+  // for (int i = 1; i <= n; ++i) {
+  //   printf("%d ", rand()%n+1);
+  // }
+  // cout<<endl;
+  return 0;
+}
+
