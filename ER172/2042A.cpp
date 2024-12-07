da6f9fd58001631c4a0e56c60fcf0bc504ae79c0
MakinoRuki <muye67@gmail.com>
2024-12-06 22:16:28 -0800

    Create 2042A.cpp

---
+++ ER172/2042A.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 500005
+#define M 405
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0)); 
+const int base=337;
+int n, m, k;
+int t;
+int a[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d%d", &n, &k);
+    for (int i = 1; i <= n; ++i) {
+      scanf("%d", &a[i]);
+    }
+    sort(a+1, a+n+1);
+    ll tot=0LL;
+    int i;
+    for (i = n; i >= 1; --i) {
+      tot += a[i];
+      if (tot >= k) break;
+    }
+    if (tot < k) printf("%lld\n", k-tot);
+    else if (k == tot) puts("0");
+    else {
+      tot -= a[i];
+      printf("%lld\n", k-tot);
+    }
+  }
+  // cin>>n;
+  // srand((unsigned)time(0)); 
+  // for (int i = 1; i <= n; ++i) {
+  //   printf("%d ", rand()%n+1);
+  // }
+  // cout<<endl;
+  return 0;
+}
+
