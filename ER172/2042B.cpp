1dab93fa848b6497634829e6e97916b0fb4f10d6
MakinoRuki <muye67@gmail.com>
2024-12-06 22:17:02 -0800

    Create 2042B.cpp

---
+++ ER172/2042B.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 500005
+#define M 405
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0)); 
+const int base=337;
+int n, m, k;
+int t;
+int a[N];
+int cnt[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d", &n);
+    for (int i = 1; i <= n; ++i) {
+      cnt[i]=0;
+    }
+    for (int i = 1; i <= n; ++i) {
+      scanf("%d", &a[i]);
+      cnt[a[i]]++;
+    }
+    int n1=0,n2=0;
+    for (int i = 1; i <= n; ++i) {
+      if (cnt[i]) {
+        if (cnt[i] <= 1) n1++;
+        else n2++;
+      }
+    }
+    int ans=n2;
+    ans += (n1+1)/2*2;
+    printf("%d\n", ans);
+  }
+  // cin>>n;
+  // srand((unsigned)time(0)); 
+  // for (int i = 1; i <= n; ++i) {
+  //   printf("%d ", rand()%n+1);
+  // }
+  // cout<<endl;
+  return 0;
+}
+
