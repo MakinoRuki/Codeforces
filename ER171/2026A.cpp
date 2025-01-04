ed50041d28568167312fc1d52b855433890acd1e
MakinoRuki <muye67@gmail.com>
2025-01-04 02:57:12 -0800

    Create 2026A.cpp

---
+++ ER171/2026A.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 300005
+#define M 2010
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0)); 
+const int base=337;
+int n, m, q;
+int k;
+int t;
+ll dp[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+      int x,y;
+      scanf("%d%d%d", &x, &y, &k);
+      int z=min(x,y);
+      printf("%d %d %d %d\n", 0, 0, z, z);
+      printf("%d %d %d %d\n", 0, z, z, 0);
+  }
+  // cin>>n;
+  // srand((unsigned)time(0)); 
+  // for (int i = 1; i <= n; ++i) {
+  //   printf("%d ", rand()%n+1);
+  // }
+  // cout<<endl;
+  return 0;
+}
