b699a8a75f6295c8698b77d5461bb0bd7514c3ca
MakinoRuki <muye67@gmail.com>
2025-02-21 02:24:54 -0800

    Create 2067A.cpp

---
+++ R1004/2067A.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 500005
+#define M 1005
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const int mod2 = 998244353;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0)); 
+const int base=337;
+int n, m, q;
+int k;
+int t;
+ll a[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    int x,y;
+    scanf("%d%d", &x, &y);
+    int dt = x-y+1;
+    if (dt % 9) {
+      puts("No");
+    } else {
+      dt /= 9;
+      if (dt >= 0) {
+        puts("Yes");
+      } else {
+        puts("No");
+      }
+    }
+  }
+
+  // cin>>n;
+  // srand((unsigned)time(0)); 
+  // for (int i = 1; i <= n; ++i) {
+  //   printf("%d ", rand()%n+1);
+  // }
+  // cout<<endl;
+  return 0;
+}
