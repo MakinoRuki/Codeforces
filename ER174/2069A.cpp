84a069492db531c05368c0ee5c9d294c9131b1aa
MakinoRuki <muye67@gmail.com>
2025-02-21 02:50:55 -0800

    Create 2069A.cpp

---
+++ ER174/2069A.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 200005
+#define M 1005
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const int mod2 = 998244353;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0)); 
+const int base=337;
+int n, m, q;
+int k;
+int t;
+int a[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d", &n);
+    n-=2;
+    for (int i = 1; i <= n; ++i) {
+      scanf("%d", &a[i]);
+    }
+    bool ok=true;
+    for (int i = 1; i <= n; ++i) {
+      if (a[i] == 1) {
+        if (i+1<=n && a[i+1]==0) {
+          if (i+2<=n && a[i+2]!=0) {
+            ok=false;
+            break;
+          }
+        }
+      }
+    }
+    if (ok) puts("YES");
+    else puts("NO");
+  }
+  // cin>>n;
+  // srand((unsigned)time(0)); 
+  // for (int i = 1; i <= n; ++i) {
+  //   printf("%d ", rand()%n+1);
+  // }
+  // cout<<endl;
+  return 0;
+}
