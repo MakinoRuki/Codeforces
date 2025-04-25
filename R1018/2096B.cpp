30cbfd5f0ca2eb6c5a3c602c31c2cbb88cb5d437
MakinoRuki <muye67@gmail.com>
2025-04-25 18:43:43 +0900

    Create 2096B.cpp

---
+++ R1018/2096B.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 600005
+#define M 750
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const int mod2 = 998244353;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0)); 
+const int base=337;
+int n, m, q;
+int k;
+int t;
+int a[N];
+int b[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d%d", &n, &k);
+    vector<int> v;
+    v.clear();
+    for (int i = 1; i <= n; ++i) {
+        scanf("%d", &a[i]);
+    }
+    for (int i = 1; i <= n; ++i) {
+        scanf("%d", &b[i]);
+    }
+    ll ans = 0LL;
+    for (int i = 1; i <= n; ++i) {
+        ans += max(a[i], b[i]);
+        v.push_back(min(a[i], b[i]));
+    }
+    sort(v.begin(), v.end());
+    for (int i = 1; i < k; ++i) {
+        ans += v[(int)v.size()-i];
+    }
+    ans++;
+    printf("%lld\n", ans);
+  }
+  // cin>>n;
+  // srand((unsigned)time(0)); 
+  // for (int i = 1; i <= n; ++i) {
+  //   printf("%d ", rand()%n+1);
+  // }
+  // cout<<endl;
+  return 0;
+}
