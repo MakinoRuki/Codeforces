fd01eefb10051a46edbcf87e828bb1586cfe7a60
MakinoRuki <muye67@gmail.com>
2025-05-09 10:52:09 -0700

    Create 2107B.cpp

---
+++ R1023/2107B.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 500005
+#define M 1002
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const int mod2 = 998244353;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0)); 
+const int base=337;
+int n, m, q;
+int k;
+int t;
+int a[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d%d", &n, &k);
+    ll tot=0LL;
+    for (int i = 1; i <= n; ++i) {
+        scanf("%d", &a[i]);
+        tot += a[i];
+    }
+    sort(a+1, a+n+1);
+    a[n]--;
+    sort(a+1, a+n+1);
+    int dt = a[n]-a[1];
+    if (dt > k) {
+        puts("Jerry");
+    } else {
+        if (tot&1) puts("Tom");
+        else puts("Jerry");
+    }
+  }
+
+  // cin>>n;
+  // srand((unsigned)time(0)); 
+  // for (int i = 1; i <= n; ++i) {
+  //   printf("%d ", rand()%n+1);
+  // }
+  // cout<<endl;
+  return 0;
+}
