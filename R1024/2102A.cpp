a142028da0f6b05510a2f7c8ef77ce3fb1f95d02
MakinoRuki <muye67@gmail.com>
2025-05-14 21:43:11 +0900

    Create 2102A.cpp

---
+++ R1024/2102A.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 200005
+#define M 1002
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const int mod2 = 998244353;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0)); 
+const int base=337;
+int n, m, p, q;
+int k;
+int t;
+int a[N];
+int cnt[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d%d%d%d", &n, &m, &p, &q);
+    if (n%p) {
+        puts("Yes");
+    } else {
+        int dt = n/p;
+        if ((dt * q) == m) {
+            puts("Yes");
+        } else {
+            puts("No");
+        }
+    }
+  }
+
+  // cin>>n;
+  // srand((unsigned)time(0)); 
+  // for (int i = 1; i <= n; ++i) {
+  //   printf("%d ", rand()%n+1);
+  // }
+  // cout<<endl;
+  return 0;
+}
