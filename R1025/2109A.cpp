eb49980c5449e10c6db68e2b0704e5fd9bedadf6
MakinoRuki <muye67@gmail.com>
2025-05-31 08:24:25 -0700

    Create 2109A.cpp

---
+++ R1025/2109A.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 500005
+#define M 1002
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const int mod2 = 998244353;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0)); 
+const int base=337;
+int n, m, q;
+ll k;
+int t;
+int a[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d", &n);
+    for (int i = 1; i <= n; ++i) {
+      scanf("%d", &a[i]);
+    }
+    int n0=0,n1=0;
+    bool ok=true;
+    for (int i = 1; i <= n; ++i) {
+        if (a[i]) n1++;
+        else {
+            n0++;
+            if (i-1>=1 && a[i-1]==0) {
+                ok=false;
+                break;
+            }
+        }
+    }
+    if (!ok) puts("Yes");
+    else {
+        if (n1 == n || n0 == n) {
+            puts("Yes");
+        } else {
+            puts("No");
+        }
+    }
+  }
+
+  // cin>>n;
+  // srand((unsigned)time(0)); 
+  // for (int i = 1; i <= n; ++i) {
+  //   printf("%d ", rand()%n+1);
+  // }
+  // cout<<endl;
+  return 0;
+}
