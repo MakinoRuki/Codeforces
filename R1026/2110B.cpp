bc9f69d372cbda8cab3d409a4599368920315d02
MakinoRuki <muye67@gmail.com>
2025-05-31 08:45:55 -0700

    Create 2110B.cpp

---
+++ R1026/2110B.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 500005
+#define M 1002
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const int mod2 = 998244353;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0)); 
+const int base=337;
+int n, m, q;
+ll k;
+int t;
+int a[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    string s;
+    cin>>s;
+    int n = (int)s.size();
+    int sum=0;
+    int i;
+    for (i = 0; i < n; ++i) {
+        if (s[i] == '(') sum++;
+        else sum--;
+        if (sum == 0) break;
+    }
+    if (i >= n-1) {
+        puts("No");
+    } else {
+        puts("Yes");
+    }
+  }
+
+  // cin>>n;
+  // srand((unsigned)time(0)); 
+  // for (int i = 1; i <= n; ++i) {
+  //   printf("%d ", rand()%n+1);
+  // }
+  // cout<<endl;
+  return 0;
+}
