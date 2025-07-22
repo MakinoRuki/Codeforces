814e25fbf75a0f1ef28d2429b610e126ad2104f6
MakinoRuki <muye67@gmail.com>
2025-07-22 06:01:13 -0700

    Create 2112A.cpp

---
+++ ER180/2112A.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 1000005
+#define M 1002
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const int mod2 = 998244353;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0)); 
+const int base=337;
+int n, m, q;
+int k;
+int t;
+int a[N];
+int b[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    int x,y;
+    scanf("%d%d%d", &n, &x, &y);
+    if (x > y) swap(x, y);
+    if (n >= x && n <= y) {
+        puts("No");
+    } else {
+        puts("Yes");
+    }
+  }
+
+  // cin>>n;
+  // srand((unsigned)time(0)); 
+  // for (int i = 1; i <= n; ++i) {
+  //   printf("%d ", rand()%n+1);
+  // }
+  // cout<<endl;
+  return 0;
+} 
