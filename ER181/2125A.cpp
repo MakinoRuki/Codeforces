19567e2f675d218568715bac759182deb21ca4d8
MakinoRuki <muye67@gmail.com>
2025-08-02 17:01:00 +0900

    Create 2125A.cpp

---
+++ ER181/2125A.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 600005
+#define M 1002
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const int mod2 = 998244353;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0)); 
+const int base=337;
+int n, m, q;
+int k;
+int t;
+int sx, sy, ex, ey;
+int a[N];
+int cnt[30];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    string s;
+    cin>>s;
+    for (int i = 0; i < 26; ++i) {
+      cnt[i]=0;
+    }
+    n = (int)s.size();
+    for (int i = 0; i < n; ++i) {
+      cnt[s[i]-'A']++;
+    }
+    string ans="";
+    if (cnt['T'-'A'] > 0) {
+      ans += string(cnt['T'-'A'], 'T');
+    }
+    if (cnt['F'-'A'] > 0) {
+      ans += string(cnt['F'-'A'], 'F');
+    }
+    if (cnt['N'-'A'] > 0) {
+      ans += string(cnt['N'-'A'], 'N');
+    }
+    for (int i = 0; i < n; ++i) {
+      if (s[i] != 'T' && s[i] != 'N' && s[i] != 'F') {
+        ans.push_back(s[i]);
+      }
+    }
+    cout<<ans<<endl;
+  }
+
+  // cin>>n;
+  // srand((unsigned)time(0)); 
+  // for (int i = 1; i <= n; ++i) {
+  //   printf("%d ", rand()%n+1);
+  // }
+  // cout<<endl;
+  return 0;
+} 
