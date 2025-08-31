c08413461524dcd8bacb21f4219678d0cc256420
MakinoRuki <muye67@gmail.com>
2025-08-31 15:07:39 +0900

    Create 2133B.cpp

---
+++ R1044/2133B.cpp
@@ -0,0 +1,56 @@
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 600005
+#define M 5002
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const int mod2 = 998244353;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0)); 
+const int base=337;
+int n, m, q;
+int k;
+int t;
+int a[N];
+int main() {
+  //freopen("data.txt", "r", stdin);
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d", &n);
+    for (int i = 1; i <= n; ++i) {
+      scanf("%d", &a[i]);
+    }
+    sort(a+1, a+n+1);
+    ll ans=0LL;
+    for (int i = n; i >= 1; i -= 2) {
+      ans += a[i];
+    }
+    printf("%lld\n", ans);
+  }
+
+  // cin>>n;
+  // srand((unsigned)time(0)); 
+  // for (int i = 1; i <= n; ++i) {
+  //   printf("%d ", rand()%n+1);
+  // }
+  // cout<<endl;
+  return 0;
+} 
