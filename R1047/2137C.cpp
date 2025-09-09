3b947c9dd5440e8ecebae192cf170292585d5188
MakinoRuki <muye67@gmail.com>
2025-09-09 04:08:21 -0700

    Create 2137C.cpp

---
+++ R1047/2137C.cpp
@@ -0,0 +1,62 @@
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 600005
+#define M 5002
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const int mod2 = 998244353;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0)); 
+const int base=337;
+int n, m, q;
+int k;
+int t;
+int a[N];
+int main() {
+ // freopen("data.txt", "r", stdin);
+  cin>>t;
+  for (int cas = 1; cas <= t; ++cas) {
+    ll x,y;
+    scanf("%lld%lld", &x, &y);
+    if (y%2) {
+      if ((x%2) == 0) {
+        puts("-1");
+      } else {
+        printf("%lld\n", x*y+1);
+      }
+    } else {
+      if (x%2) {
+        if (y%4) puts("-1");
+        else printf("%lld\n", x*y/2+2);
+      } else {
+        printf("%lld\n", x*y/2+2);
+      }
+    }
+  }
+
+  // cin>>n;
+  // srand((unsigned)time(0)); 
+  // for (int i = 1; i <= n; ++i) {
+  //   printf("%d ", rand()%n+1);
+  // }
+  // cout<<endl;
+  return 0;
+} 
