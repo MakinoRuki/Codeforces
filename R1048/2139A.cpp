aa3ee328f4dbe5d8dbb8d106068c3c7fb040f596
MakinoRuki <muye67@gmail.com>
2025-09-15 05:32:26 -0700

    Create 2139A.cpp

---
+++ R1048/2139A.cpp
@@ -0,0 +1,62 @@
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 600005
+#define M 5002
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const int mod2 = 998244353;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0)); 
+const int base=337;
+int n, m, q;
+int k;
+int t;
+int a[N];
+int getgcd(int x, int y) {
+  return y == 0? x : getgcd(y, x%y);
+}
+int main() {
+ // freopen("data.txt", "r", stdin);
+  cin>>t;
+  for (int cas = 1; cas <= t; ++cas) {
+    int x,y;
+    scanf("%d%d", &x, &y);
+    if (x==y) {
+      puts("0");
+      continue;
+    }
+    int gcd=getgcd(x,y);
+    int lcm=x*y/gcd;
+    if (lcm == x || lcm == y) {
+      puts("1");
+    } else {
+      puts("2");
+    }
+  }
+
+  // cin>>n;
+  // srand((unsigned)time(0)); 
+  // for (int i = 1; i <= n; ++i) {
+  //   printf("%d ", rand()%n+1);
+  // }
+  // cout<<endl;
+  return 0;
+} 
