af523dbd05e532f9f26f32f392ec87faf1240d76
MakinoRuki <muye67@gmail.com>
2025-10-04 18:19:32 +0900

    Create 2151A.cpp

---
+++ R1053/2151A.cpp
@@ -0,0 +1,57 @@
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 600005
+#define M 305
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const int mod2 = 998244353;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0));
+const int base=337;
+int n, m, q;
+int k;
+int t;
+int a[N];
+int main() {
+ // freopen("data.txt", "r", stdin);
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    int l, r;
+    scanf("%d%d", &n, &m);
+    int id1=0;
+    for (int i = 1; i <= m; ++i) {
+      scanf("%d", &a[i]);
+      if (a[i] == 1) id1=i;
+    }
+    if (id1 > 1) {
+      puts("1");
+      continue;
+    }
+    printf("%d\n", n-a[m]+1);
+  }
+  // cin>>n;
+  // srand((unsigned)time(0));
+  // for (int i = 1; i <= n; ++i) {
+  //   printf("%d ", rand()%n+1);
+  // }
+  // cout<<endl;
+  return 0;
+}
