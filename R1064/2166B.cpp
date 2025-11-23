d58a45da7046ae961e5f6371e8c12e15d929d604
MakinoRuki <muye67@gmail.com>
2025-11-23 16:51:34 +0900

    Create 2166B.cpp

---
+++ R1064/2166B.cpp
@@ -0,0 +1,54 @@
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 2000005
+#define M 5002
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const int mod2 = 998244353;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0));
+const int base=337;
+int n, m, q;
+int k;
+int t;
+int a[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+      int x,y;
+      scanf("%d%d%d", &x, &y, &n);
+      int res;
+      if (x/n >= y) {
+        res = 1;
+      } else {
+        if (x==y) res=1;
+        else res=2;
+      }
+      printf("%d\n", res);
+  }
+  // cin>>n;
+  // srand((unsigned)time(0));
+  // for (int i = 1; i <= n; ++i) {
+  //   printf("%d ", rand()%n+1);
+  // }
+  // cout<<endl;
+  return 0;
+}
