8984dff938ab86f4235581c65f63a1b53e2bbb89
MakinoRuki <muye67@gmail.com>
2025-12-14 21:58:41 +0900

    Create 2176B.cpp

---
+++ R1070/2176B.cpp
@@ -0,0 +1,70 @@
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 500005
+#define M 8002
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const int mod2 = 998244353;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0)); 
+const int base=337;
+int n, m, q;
+int k;
+int t;
+int a[N];
+bool vis[N];
+int nxt[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d", &n);
+    string s;
+    cin>>s;
+    nxt[n]=-1;
+    for (int i = n-1; i >= 0; --i) {
+      if (s[i] == '1') nxt[i] = i;
+      else nxt[i] = nxt[i+1];
+    }
+    int bg=-1;
+    for (int i = 0; i < n; ++i) {
+      if (s[i] == '1') {
+        bg = i;
+        break;
+      }
+    }
+    int ans=0;
+    for (int i = n-1; i >= 0; --i) {
+      if (nxt[i] == -1) {
+        ans = max(ans, n-i+bg);
+      } else {
+        ans = max(ans, nxt[i]-i);
+      }
+    }
+    printf("%d\n", ans);
+  }
+  // cin>>n;
+  // srand((unsigned)time(0)); 
+  // for (int i = 1; i <= n; ++i) {
+  //   printf("%d ", rand()%n+1);
+  // }
+  // cout<<endl;
+  return 0;
+} 
