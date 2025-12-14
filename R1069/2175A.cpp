faa7e47a58d28d15a5298c18d344f7372c889d6f
MakinoRuki <muye67@gmail.com>
2025-12-14 21:33:58 +0900

    Create 2175A.cpp

---
+++ R1069/2175A.cpp
@@ -0,0 +1,75 @@
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 500005
+#define M 5002
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const int mod2 = 998244353;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0));
+const int base=337;
+int n, m, q;
+int k;
+int t;
+int a[N];
+int b[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d", &n);
+    set<int> ss;
+    ss.clear();
+    for (int i = 1; i <= n; ++i) {
+      scanf("%d", &a[i]);
+      ss.insert(a[i]);
+    }
+    int sz = (int)ss.size();
+    sort(a+1, a+n+1);
+    ll ans = sz;
+    if (sz < a[1]) {
+      printf("%d\n", a[1]);
+      continue;
+    }
+    if (sz > a[n]) {
+      ll dt = 1000000000000000000LL-n;
+      printf("%lld\n", sz + dt-1);
+      continue;
+    }
+    for (int i= 1; i <= n; ++i) {
+      if (sz == a[i]) {
+        ans = a[i];
+        break;
+      }
+      if (i+1<=n && sz > a[i] && sz < a[i+1]) {
+        ans = a[i+1];
+        break;
+      }
+    }
+    printf("%lld\n", ans);
+  } 
+  // cin>>n;
+  // srand((unsigned)time(0));
+  // for (int i = 1; i <= n; ++i) {
+  //   printf("%d ", rand()%n+1);
+  // }
+  // cout<<endl;
+  return 0;
+}
