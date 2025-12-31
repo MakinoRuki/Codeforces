847ada37200808f8913701acbeda970eead6d2c9
MakinoRuki <muye67@gmail.com>
2025-12-31 22:49:16 +0900

    Create 2182A.cpp

---
+++ ER186/2182A.cpp
@@ -0,0 +1,63 @@
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 500005
+#define M 8002
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const int mod2 = 998244353;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0)); 
+const int base=337;
+int n, m, q;
+int k;
+int t;
+ll a[N];
+int main() {
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d", &n);
+    string s;
+    cin>>s;
+    int ans=0;
+    bool found=false;
+    for (int i = 0; i < n; ++i) {
+      if (i+3 < n && s.substr(i, 4) == "2025") {
+        ans++;
+      }
+      if (i + 3 < n && s.substr(i, 4) == "2026") {
+        found=true;
+      }
+    }
+    if (found) puts("0");
+    else if (ans == 0) {
+      puts("0");
+    } else {
+      puts("1");
+    }
+  }
+  // cin>>n;
+  // srand((unsigned)time(0)); 
+  // for (int i = 1; i <= n; ++i) {
+  //   printf("%d ", rand()%n+1);
+  // }
+  // cout<<endl;
+  return 0;
+} 
