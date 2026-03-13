452bdc3cfc7b76501c0f71b609ef28dba9a5db3a
MakinoRuki <muye67@gmail.com>
2026-03-13 20:29:42 +0900

    Create 2205A.cpp

---
+++ R1083/2205A.cpp
@@ -0,0 +1,61 @@
+
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 500005
+#define M 505
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const ll mod3 = 1000003;
+const ll base1 = 13331LL;
+const ll base2 = 23333LL;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0));
+const int base=337;
+int n, m, q;
+int k, w;
+int t;
+int a[N];
+int mx[N][2];
+int res[M][M];
+int main() {
+  // cin>>t;
+  // for (int cas=1; cas<=t; ++cas) {
+  // }
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    int h;
+    int x,y;
+    scanf("%d", &n);
+    int id=0;
+    for (int i =1; i <= n; ++i) {
+      scanf("%d", &a[i]);
+      if (a[i] == n) id = i;
+    }
+    swap(a[1], a[id]);
+    for (int i =1; i <= n; ++i) {
+      printf("%d ", a[i]);
+    }
+    cout<<endl;
+  }
+  return 0;
+}
+
