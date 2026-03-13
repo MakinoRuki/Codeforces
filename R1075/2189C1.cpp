4e22cefdde17d6a52057fe0966cfd34cabe6ec3a
MakinoRuki <muye67@gmail.com>
2026-03-13 15:23:01 +0900

    Create 2189C1.cpp

---
+++ R1075/2189C1.cpp
@@ -0,0 +1,64 @@
+
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 3000005
+#define M 1005
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod1 = 1000000007LL;
+const ll mod2 = 998244353LL;
+const ll mod3 = 1000003;
+const ll base1 = 13331LL;
+const ll base2 = 23333LL;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0));
+const int base=337;
+int n, m, q;
+int k, w;
+int t;
+int a[N];
+ll b[N];
+ll c[N];
+int main() {
+  // cin>>t;
+  // for (int cas=1; cas<=t; ++cas) {
+  // }
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d", &n);
+    a[n]=1;
+    set<int> ss;
+    ss.clear();
+    for (int i =2; i <= n; ++i) {
+      ss.insert(i);
+    }
+    for (int i = 2; i < n; ++i) {
+      a[i] = (i%2) ? i-1 : i+1;
+      ss.erase(a[i]);
+    }
+    a[1] = *ss.begin();
+    for (int i =1; i <= n; ++i) {
+      printf("%d ", a[i]);
+    }
+    cout<<endl;
+  }
+  return 0;
+}
+
