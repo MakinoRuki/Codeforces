06badcbc4c2447b0499f176dbb6b2ababe753947
MakinoRuki <muye67@gmail.com>
2026-04-04 19:42:23 +0900

    Create 2211A.cpp

---
+++ R1088/2211A.cpp
@@ -0,0 +1,65 @@
+
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 200005
+#define M 110
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const ll mod3 = 1000003;
+const ll base1 = 13331LL;
+const ll base2 = 23333LL;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0));
+const int base=337;
+int n, m, q;
+int k, w;
+int t;
+int a[N];
+int b[N];
+vector<string> v;
+int main() {
+  // cin>>t;
+  // for (int cas=1; cas<=t; ++cas) {
+  // }
+  // ll ans=1LL;
+  // for (int i =1; i <= 500; ++i) {
+  //   ans=ans*5813LL%9422LL;
+  // }
+  // cout<<ans<<endl;
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    scanf("%d", &n);
+    for (int i = 1; i <= n; ++i) {
+      scanf("%d", &a[i]);
+    }
+    if (n == 1) {
+      puts("1");
+      continue;
+    }
+    for (int i = 1; i <= n; ++i) {
+      printf("2 ");
+    }
+    cout<<endl;
+  }
+  return 0;
+}
+
