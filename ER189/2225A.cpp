f5e7bf72f718ac1546232141aa5f398916195082
MakinoRuki <muye67@gmail.com>
2026-05-09 21:16:41 +0900

    Create 2225A.cpp

---
+++ ER189/2225A.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 500005
+#define M 110
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const ll mod3 = 676767677;
+const ll base1 = 13331LL;
+const ll base2 = 23333LL;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0));
+const int base=337;
+int n, m, q;
+int k, w;
+int t;
+int a[N];
+int p;
+int main() {
+  // cin>>t;
+  // for (int cas=1; cas<=t; ++cas) {
+  // }
+  // ll ans=1LL;
+  // for (int i =1; i <= 500; ++i) {
+  //   ans=ans*5813LL%9422LL;
+  // }
+  // cout<<ans<<endl;
+  cin>>t;
+  for (int cas=1; cas<=t; ++cas) {
+    ll x,y;
+    scanf("%lld%lld", &x, &y);
+    ll dt = y/x;
+    ll xx = (dt-1)*x;
+    if (dt-1 >= 2 && (y%xx)) {
+      puts("YES");
+    } else {
+      puts("NO");
+    }
+  }
+  return 0;
+}
+
