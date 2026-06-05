dac18a79ba50adf09fe54150bb325e6fab9d4328
MakinoRuki <muye67@gmail.com>
2026-06-05 22:20:10 +0900

    Create 2229C1.cpp

---
+++ R1100/2229C1.cpp
@@ -0,0 +1,76 @@
+
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 1000005
+#define M 5002
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const ll mod3 = 676767677;
+const ll base1 = 13331LL;
+const ll base2 = 23333LL;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0));
+const int base=337;
+int n, m, q;
+int k, w;
+int t;
+int a[N];
+int b[N];
+int main() {
+  // cin>>t;
+  // for (int cas=1; cas<=t; ++cas) {
+  // }
+  // ll ans=1LL;
+  // for (int i =1; i <= 500; ++i) {
+  //   ans=ans*5813LL%9422LL;
+  // }
+  // cout<<ans<<endl;
+ cin>>t;
+ for (int cas=1; cas<=t; ++cas) {
+   ll x;
+   scanf("%d", &n);
+   for (int i = 1; i <= n; ++i) {
+     scanf("%d", &a[i]);
+   }
+   vector<int> v;
+   v.clear();
+   int cur = 0;
+   for (int i = n; i >= 1; --i) {
+     if (a[i] < 0 && cur) {
+       v.push_back(i);
+       cur = !cur;
+     } else if (a[i] > 0 && !cur) {
+       v.push_back(i);
+       cur = !cur;
+     }
+   }
+   int sz=(int)v.size();
+   printf("%d\n", sz);
+   for (auto x : v) {
+     printf("%d ", x);
+   }
+   cout<<endl;
+ }
+ return 0;
+}
+
+
