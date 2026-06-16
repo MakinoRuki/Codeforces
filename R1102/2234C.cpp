11ba06a58aff1448f68dea6763de1f269e74c7b8
MakinoRuki <muye67@gmail.com>
2026-06-16 20:50:04 +0900

    Create 2234C.cpp

---
+++ R1102/2234C.cpp
@@ -0,0 +1,85 @@
+
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <random>
+#include <ctime>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <chrono>
+#define N 2000005
+#define M 5002
+#define eps 1e-7
+using namespace std;
+typedef long long ll;
+const ll mod = 1000000007LL;
+const ll mod2 = 998244353LL;
+const ll mod3 = 676767677;
+const ll base1 = 13331LL;
+const ll base2 = 23333LL;
+const int inf = 1000000010;
+//const __int128 hmod = 212370440130137957LL;
+//srand((unsigned)time(0));
+const int base=337;
+int n, m, q;
+int k, w;
+int t;
+int a[N];
+int b[N];
+int main() {
+  // cin>>t;
+  // for (int cas=1; cas<=t; ++cas) {
+  // }
+  // ll ans=1LL;
+  // for (int i =1; i <= 500; ++i) {
+  //   ans=ans*5813LL%9422LL;
+  // }
+  // cout<<ans<<endl;
+ cin>>t;
+ for (int cas=1; cas<=t; ++cas) {
+ //  ll x, y;
+  // ll n;
+   scanf("%d", &n);
+  //  string s;
+  //  cin>>s;
+   for (int i = 0; i < n; ++i) {
+     scanf("%d", &a[i]);
+   }
+   for (int i = 0; i < n; ++i) {
+     b[i] = 0;
+     int mx = a[i];
+     for (int j = 1; j < n; ++j) {
+       int r = (i+j)%n;
+       b[r] = mx;
+       mx = max(mx, a[r]);
+       //if (i == 1) cout<<"r="<<r<<" "<<b[r]<<" "<<mx<<endl;
+     }
+     mx = a[(i-1+n)%n];
+     for (int j = 1; j < n; ++j) {
+       int r = (i-j+n)%n;
+       b[r] = min(b[r], mx);
+       mx = max(mx, a[(r-1+n)%n]);
+      // if (i == 1) cout<<"r="<<r<<" "<<b[r]<<" "<<mx<<endl;
+     }
+     ll ans= 0LL;
+     for (int j = 1; j < n; ++j) {
+       int r = (i+j)%n;
+       ans += b[r];
+     }
+     printf("%lld ", ans);
+   }
+   cout<<endl;
+ }
+ return 0;
+}
+
+
